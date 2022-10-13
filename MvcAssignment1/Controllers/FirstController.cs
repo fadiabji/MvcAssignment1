2365aea8d6d0e1ef302b8e6b843ebbf423f2490c
fadiabji <fadi.abji@htomail.com>
2022-10-13 14:14:06 +0200

    Add project files.

---
+++ MvcAssignment1/Controllers/FirstController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcAssignment1.Controllers
+{
+    public class FirstController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Msg = "Here is using ViewBag FirstController Index!";
+            ViewData["Message"] = "Here is using ViewData FirstController Index!";
+            TempData["Message"] = "Here is using TempData FirstController Index!";
+            return View();
+        }
+
+        public IActionResult controllerOneFirstMethod()
+        {
+            ViewBag.Msg = "Here is using ViewBag FirstController controllerOneFirstMethod!";
+            ViewData["Message"] = "Here is using ViewData FirstController controllerOneFirstMethod!";
+            TempData["Message"] = "Here is using TempData FirstController controllerOneFirstMethod!";
+            return View();
+        }
+
+        public IActionResult controllerOneSecondMethod()
+        {
+            ViewBag.Msg = "Here is using ViewBag FirstController controllerTwoSecondMethod!";
+            ViewData["Message"] = "Here is using ViewData FirstController controllerTwoSecondMethod!";
+            TempData["Message"] = "Here is using TempData FirstController controllerTwoSecondMethod!";
+
+            return View();
+        }
+    }
+}
---
+++ MvcAssignment1/Controllers/SecondController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcAssignment1.Controllers
+{
+    public class SecondController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Msg = "Here is using ViewBag SecondController Index!";
+            ViewData["Message"] = "Here is using ViewData SecondController Index!";
+            TempData["Message"] = "Here is using TempData SecondController Index!";
+
+            return View();
+        }
+
+        public IActionResult secondControllerFirstMethod()
+        {
+            ViewBag.Msg = "Here is using ViewBag SecondController secondControllerFirstMethod!";
+            ViewData["Message"] = "Here is using ViewData SecondController secondControllerFirstMethod!";
+            TempData["Message"] = "Here is using TempData SecondController secondControllerFirstMethod!";
+
+            return View();
+        }
+
+        public IActionResult secondControllerSecondMethod()
+        {
+            ViewBag.Msg = "Here is using ViewBag SecondController secondControllerSecondMethod!";
+            ViewData["Message"] = "Here is using ViewData SecondController secondControllerSecondMethod!";
+            TempData["Message"] = "Here is using TempData SecondController secondControllerSecondMethod!";
+
+            return View();
+        }
+    }
+}
